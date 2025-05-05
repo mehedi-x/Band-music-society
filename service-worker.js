b71d70f6e0d3728a96592be28dc8048d932340ab
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-06 02:19:28 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('russian-app').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
