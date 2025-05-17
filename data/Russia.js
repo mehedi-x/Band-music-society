333c8b698b85f8cbc4c223468842284fe2bc7b12
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-18 00:51:18 +0600

    Create Russia.js

---
+++ data/Russia.js
@@ -0,0 +1,5 @@
+const Russia = [
+  { native: "Дом", pron: "দম", meaning: "বাড়ি" },
+  { native: "Книга", pron: "কনিগা", meaning: "বই" },
+  { native: "Привет", pron: "প্রিভিয়েট", meaning: "হ্যালো" }
+];
