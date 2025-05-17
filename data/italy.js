7a10f313c92cc73638c260b45d4d351a951e98f2
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-18 01:37:12 +0600

    Delete data directory

--- data/italy.js
+++
@@ -1,4 +0,0 @@
-const France = [
-  { native: "Maison", pron: "মেজঁ", meaning: "বাড়ি" },
-  { native: "Livre", pron: "লিভর", meaning: "বই" }
-];
--- data/russia.js
+++
@@ -1,5 +0,0 @@
-const Russia = [
-  { native: "Дом", pron: "দম", meaning: "বাড়ি" },
-  { native: "Книга", pron: "কনিগা", meaning: "বই" },
-  { native: "Вода", pron: "ভদা", meaning: "পানি" }
-];
