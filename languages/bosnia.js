95cccda42a30a7586b252c322c04d31d55a6c452
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:03:13 +0600

    Create bosnia.js

---
+++ languages/bosnia.js
@@ -0,0 +1,4 @@
+const BOSNIA_DATA = [
+  { en: "What is your name?", bn: "আপনার নাম কী?", bos: "Kako se zoveš?" },
+  { en: "I don't understand", bn: "আমি বুঝতে পারছি না", bos: "Ne razumijem" },
+];
