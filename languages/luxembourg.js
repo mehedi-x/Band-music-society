c89f18ddd8791184015d5d001e5ab78bc1c38c2e
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:13:18 +0600

    Create luxembourg.js

---
+++ languages/luxembourg.js
@@ -0,0 +1,11 @@
+export const data = {
+  country: "Luxembourg",
+  language: "Luxembourgish, French, German",
+  phrases: {
+    hello: "Moien / Bonjour / Hallo",
+    thankYou: "Merci / Danke",
+    goodMorning: "Gudde Moien / Bonjour / Guten Morgen",
+    yes: "Jo / Oui / Ja",
+    no: "Nee / Non / Nein"
+  }
+};
