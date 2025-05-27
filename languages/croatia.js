45dd4cb83aba1ae98f42b0494b02af881fc6443c
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:11:59 +0600

    Create croatia.js

---
+++ languages/croatia.js
@@ -0,0 +1,11 @@
+export const data = {
+  country: "Croatia",
+  language: "Croatian",
+  phrases: {
+    hello: "Bok",
+    thankYou: "Hvala",
+    goodMorning: "Dobro jutro",
+    yes: "Da",
+    no: "Ne"
+  }
+};
