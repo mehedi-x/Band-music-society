28ebf8626c7f815461a1f7cd9d759307cfad9127
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:12:16 +0600

    Create estonia.js

---
+++ languages/estonia.js
@@ -0,0 +1,11 @@
+export const data = {
+  country: "Estonia",
+  language: "Estonian",
+  phrases: {
+    hello: "Tere",
+    thankYou: "Aitäh",
+    goodMorning: "Tere hommikust",
+    yes: "Jah",
+    no: "Ei"
+  }
+};
