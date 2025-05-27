d8b3e42179fbd49d631d8f90026831de8cfd6a8a
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:04:45 +0600

    Create moldova.js

---
+++ languages/moldova.js
@@ -0,0 +1,4 @@
+const MOLDOVA_DATA = [
+  { en: "I'm sorry", bn: "আমি দুঃখিত", mol: "Îmi pare rău" },
+  { en: "Can you help me?", bn: "আপনি কি আমাকে সাহায্য করতে পারেন?", mol: "Mă poți ajuta?" },
+];
