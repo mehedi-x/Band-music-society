d2a3497fae634211cc35d9f7fb39605bb21908b5
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:04:21 +0600

    Create kosovo.js

---
+++ languages/kosovo.js
@@ -0,0 +1,4 @@
+const KOSOVO_DATA = [
+  { en: "Nice to meet you", bn: "আপনার সাথে দেখা করে ভালো লাগলো", kos: "Kënaqësi që ju njoh" },
+  { en: "Good luck", bn: "শুভ কামনা", kos: "Fat të mbarë" },
+];
