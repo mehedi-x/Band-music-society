16b9ab285c6e0d351db278614a646788a47f9892
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:13:37 +0600

    Create slovenia.js

---
+++ languages/slovenia.js
@@ -0,0 +1,11 @@
+export const data = {
+  country: "Slovenia",
+  language: "Slovenian",
+  phrases: {
+    hello: "Živjo",
+    thankYou: "Hvala",
+    goodMorning: "Dobro jutro",
+    yes: "Da",
+    no: "Ne"
+  }
+};
