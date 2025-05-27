99edcc82ed048fc9457b18a2636dce2d459270dd
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:12:41 +0600

    Create iceland.js

---
+++ languages/iceland.js
@@ -0,0 +1,11 @@
+export const data = {
+  country: "Iceland",
+  language: "Icelandic",
+  phrases: {
+    hello: "Halló",
+    thankYou: "Takk",
+    goodMorning: "Góðan daginn",
+    yes: "Já",
+    no: "Nei"
+  }
+};
