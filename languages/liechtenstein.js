621dafab071385472e1616fb09a8beced1fdcfef
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:13:02 +0600

    Create liechtenstein.js

---
+++ languages/liechtenstein.js
@@ -0,0 +1,11 @@
+export const data = {
+  country: "Liechtenstein",
+  language: "German",
+  phrases: {
+    hello: "Hallo",
+    thankYou: "Danke",
+    goodMorning: "Guten Morgen",
+    yes: "Ja",
+    no: "Nein"
+  }
+};
