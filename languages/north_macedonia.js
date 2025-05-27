12285268b0c89ef0c1de265eff195ec5f52fc4f0
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:06:51 +0600

    Create north_macedonia.js

---
+++ languages/north_macedonia.js
@@ -0,0 +1,4 @@
+const NORTH_MACEDONIA_DATA = [
+  { en: "What's your name?", bn: "তোমার নাম কী?", mk: "Како се викаш?" },
+  { en: "Nice to meet you", bn: "আপনার সাথে দেখা করে ভালো লাগলো", mk: "Мило ми е што се запознавме" },
+];
