65cec7c30f8c12e11d536905d0c3a99b63d87187
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:11:39 +0600

    Create belgium.js

---
+++ languages/belgium.js
@@ -0,0 +1,11 @@
+export const data = {
+  country: "Belgium",
+  language: "Dutch, French, German",
+  phrases: {
+    hello: "Hallo / Bonjour / Hallo",
+    thankYou: "Dank u / Merci / Danke",
+    goodMorning: "Goedemorgen / Bonjour / Guten Morgen",
+    yes: "Ja / Oui / Ja",
+    no: "Nee / Non / Nein"
+  }
+};
