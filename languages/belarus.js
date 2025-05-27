1895f7cdaafbc1d9d1692e358722dc7cb0f4e7d5
Mehedi Hasan <158504182+mehedi-x@users.noreply.github.com>
2025-05-28 01:05:31 +0600

    Create belarus.js

---
+++ languages/belarus.js
@@ -0,0 +1,4 @@
+const BELARUS_DATA = [
+  { en: "Do you speak English?", bn: "আপনি কি ইংরেজি বলতে পারেন?", blr: "Вы размаўляеце па-ангельску?" },
+  { en: "Bathroom", bn: "টয়লেট", blr: "Туалет" },
+];
